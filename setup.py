d2148fc3ee514204ced1e8f1e305756a1b4069c6
Martin Troels Eberhardt <martin@darkfox.dk>
2018-02-19 00:19:24 +0100

    0.1.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='pyconnectedcars',
-    version='0.1.6',
+    version='0.1.7',
     packages=['pyconnectedcars'],
     include_package_data=True,
     python_requires='>=3',
