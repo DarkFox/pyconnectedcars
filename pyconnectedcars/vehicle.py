91ffa41f065466d2887375f5afa938e0cd9c1ad7
Martin Troels Eberhardt <martin@darkfox.dk>
2018-02-19 00:19:02 +0100

    Use only first 3 words of vehicle name, use id instead of vin for uniq_name.

--- pyconnectedcars/vehicle.py
+++ pyconnectedcars/vehicle.py
@@ -3,11 +3,12 @@ class VehicleDevice:
         self._id = data['id']
         self._vin = data['vin']
         self._raw_name = data['name']
+        self._short_name = ' '.join(self._raw_name.split()[:3])
         self._controller = controller
         self.should_poll = True
 
     def _name(self):
-        return "{} {}".format(self._raw_name, self.type)
+        return "{} {}".format(self._short_name, self.type)
 
     def _uniq_name(self):
-        return '{} {} {}'.format(self._raw_name, self._vin, self.type)
+        return '{} {} {}'.format(self._short_name, self._id, self.type)
